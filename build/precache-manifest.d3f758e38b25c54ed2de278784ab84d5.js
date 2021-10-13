842e7200c8ddadcd5d15ad31b06a2f9741bb54fb
CalvinPierce <calvinpierce99@gmail.com>
2021-10-13 19:57:29 -0400

    adding build folder

---
+++ build/precache-manifest.d3f758e38b25c54ed2de278784ab84d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff1084f26da018cc4e68",
+    "url": "/CalvinPierce/comp3104/static/css/main.7c0b8d8f.chunk.css"
+  },
+  {
+    "revision": "ff1084f26da018cc4e68",
+    "url": "/CalvinPierce/comp3104/static/js/main.ff1084f2.chunk.js"
+  },
+  {
+    "revision": "530bec02bd2b906c9d41",
+    "url": "/CalvinPierce/comp3104/static/js/runtime~main.530bec02.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/CalvinPierce/comp3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "6f93a1ee77966d15f31e7df8f4d04a97",
+    "url": "/CalvinPierce/comp3104/index.html"
+  }
+];
