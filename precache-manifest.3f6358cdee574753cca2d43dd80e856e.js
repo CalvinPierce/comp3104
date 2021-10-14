769dc6805a22f9ad06a750d327942b4a862fd7aa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-14 00:02:38 +0000

    Deploy CalvinPierce/comp3104 to github.com/CalvinPierce/comp3104.git:gh-pages

---
+++ precache-manifest.3f6358cdee574753cca2d43dd80e856e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74a58a011837c2678631",
+    "url": "/CalvinPierce/comp3104/static/css/main.b48139be.chunk.css"
+  },
+  {
+    "revision": "74a58a011837c2678631",
+    "url": "/CalvinPierce/comp3104/static/js/main.74a58a01.chunk.js"
+  },
+  {
+    "revision": "530bec02bd2b906c9d41",
+    "url": "/CalvinPierce/comp3104/static/js/runtime~main.530bec02.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/CalvinPierce/comp3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "e8b46348459b2a38fe7295f1afeecf4f",
+    "url": "/CalvinPierce/comp3104/index.html"
+  }
+];
